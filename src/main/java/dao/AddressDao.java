323676983861ae2e505aface81b586b351e1a3d8
alexandr_khoda <oleksandr.khodakovskyi@gmail.com>
2016-11-01 09:45:22 +0200

    deleted:  - dao;  - InMemOrderRepoTestConfiguration;  - InMemPizzaRepoTestConfiguration;

--- src/main/java/dao/AddressDao.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import domain.Address;
-
-public interface AddressDao {
-    Address find(Long id);
-
-    Long save(Address customer);
-}
--- src/main/java/dao/OrderDao.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import domain.Orders;
-
-public interface OrderDao {
-    Orders find(Long id);
-
-    Long save(Orders order);
-}
